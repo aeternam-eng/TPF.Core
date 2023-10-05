954eb28b884f929fd73300447f4119288f7dc053
Hugo Brandão <hugo.brandao@dtidigital.com.br>
2023-10-05 07:19:02 -0300

    a

--- .iac/main.tf
+++ .iac/main.tf
@@ -77,8 +77,8 @@ resource "azurerm_linux_web_app" "webapp" {
   }
 
   connection_string {
-    name  = DefaultConnection
-    type  = PostgreSQL
-    value = "User ID=stronzo;Password=${azurerm_postgresql_flexible_server.databaseserver.admin_password};Host=${azurerm_postgresql_flexible_server.databaseserver.fully_qualified_domain_name};Port=5432;Database=postgres;Include Error Detail=true"
+    name  = "DefaultConnection"
+    type  = "PostgreSQL"
+    value = "User ID=stronzo;Password=${azurerm_postgresql_flexible_server.databaseserver.administrator_password};Host=${azurerm_postgresql_flexible_server.databaseserver.fqdn};Port=5432;Database=postgres;Include Error Detail=true"
   }
 }
