310e04ef20f4f70686525396296e2c778634a02e
Hugo Brandão <hugo.brandao@dtidigital.com.br>
2023-10-04 22:08:04 -0300

    db password

--- .iac/main.tf
+++ .iac/main.tf
@@ -22,6 +22,37 @@ module "conventions" {
   appName     = var.resource_config.name
 }
 
+resource "azurerm_postgresql_flexible_server" "databaseserver" {
+  name = module.conventions.app_config.database_name
+
+  resource_group_name = module.conventions.resource_group_name
+  location            = var.resource_config.location
+
+  sku_name   = "B_Standard_B1ms"
+  version    = "13"
+  storage_mb = 32768
+
+  backup_retention_days = 7
+
+  administrator_login    = "stronzo"
+  administrator_password = "stronzo"
+  zone                   = "3"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "satpfcore${var.resource_config.environment}"
+  resource_group_name      = module.conventions.resource_group_name
+  location                 = var.resource_config.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storagecontainer" {
+  name                  = "fire-images"
+  storage_account_name  = azurerm_storage_account.storageaccount.name
+  container_access_type = "blob"
+}
+
 resource "azurerm_service_plan" "appserviceplan" {
   name                = module.conventions.app_config.app_service_plan_name
   location            = var.resource_config.location
@@ -37,6 +68,10 @@ resource "azurerm_linux_web_app" "webapp" {
   service_plan_id     = azurerm_service_plan.appserviceplan.id
   https_only          = true
 
+  app_settings = {
+    "ASPNETCORE_CONNECTIONSTRINGS__DEFAULTCONNECTION" : azurerm_postgresql_flexible_server.databaseserver.administrator_password
+  }
+
   site_config {
     minimum_tls_version = "1.2"
     always_on           = false
@@ -44,40 +79,4 @@ resource "azurerm_linux_web_app" "webapp" {
       dotnet_version = "6.0"
     }
   }
-}
-
-resource "random_password" "adminpassword" {
-  length  = 16
-  special = true
-}
-
-resource "azurerm_postgresql_flexible_server" "databaseserver" {
-  name = module.conventions.app_config.database_name
-
-  resource_group_name = module.conventions.resource_group_name
-  location            = azurerm_linux_web_app.webapp.location
-
-  sku_name   = "B_Standard_B1ms"
-  version    = "13"
-  storage_mb = 32768
-
-  backup_retention_days = 7
-
-  administrator_login    = "stronzo"
-  administrator_password = random_password.adminpassword.result
-  zone                   = "3"
-}
-
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = "satpfcore${var.resource_config.environment}"
-  resource_group_name      = module.conventions.resource_group_name
-  location                 = azurerm_linux_web_app.webapp.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "storagecontainer" {
-  name                  = "fire-images"
-  storage_account_name  = azurerm_storage_account.storageaccount.name
-  container_access_type = "blob"
 }
